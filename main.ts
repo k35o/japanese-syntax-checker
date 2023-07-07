59e82516ffc6c629ef901274711ca7e524bab6f8
Koki Sakano <kosakanoki@gmail.com>
2023-07-07 20:42:50 +0900

    linterの追加とその結果を返すように

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 import { Application, Router } from "https://deno.land/x/oak@v12.5.0/mod.ts";
 import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 import { z } from "https://deno.land/x/zod@v3.21.4/mod.ts";
+import { lintText } from "./textlint/checkSyntax.ts";
 
 const router = new Router();
 router
@@ -22,7 +23,11 @@ router
       return;
     }
 
-    context.response.body = parsedBody.data;
+    const resultMessage = (await lintText(parsedBody.data.text)).messages;
+    context.response.body = {
+      ...parsedBody.data,
+      messages: resultMessage,
+    };
   });
 
 const app = new Application();
