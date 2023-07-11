44fe6d6bfc5e5d0383c67031030b646eddff6c37
Koki Sakano <kosakanoki@gmail.com>
2023-07-11 19:10:08 +0900

    プラグインを読み込むように

--- api/index.ts
+++ api/index.ts
@@ -3,7 +3,18 @@ import { z } from 'zod';
 import { createLinter, loadTextlintrc } from "textlint";
 import { TextlintResult } from "@textlint/kernel";
 
-export const lintText = async (text: string): Promise<TextlintResult> => {
+// ビルド後のnode_modulesには読み込みのないパッケージが存在しない。そのためtextlint-module-resolverで解決できないことを避けるために明示的に呼び出す。
+// https://github.com/textlint/textlint/blob/578172ee8d875d157015127858312abf34aadc19/packages/%40textlint/config-loader/src/textlint-module-resolver.ts#L40
+const loadExternallintRule = async() => {
+  await Promise.all([
+    import('textlint-rule-no-dropping-the-ra'),
+    import('textlint-rule-preset-ja-technical-writing'),
+    import('textlint-rule-preset-jtf-style'),
+  ]);
+}
+
+const lintText = async (text: string): Promise<TextlintResult> => {
+  await loadExternallintRule();
   const descriptor = await loadTextlintrc();
   const linter = createLinter({
       descriptor
